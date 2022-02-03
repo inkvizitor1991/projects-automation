fb4612f34a0afc3f5679ff519aabfff0da618748
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2022-02-03 20:26:11 +0300

    Reduced the code

--- scripts/add_commands_to_db.py
+++ scripts/add_commands_to_db.py
@@ -1,6 +1,9 @@
-from .form_groups import form_gorups
-from automation.models import Project, Category, Student, ProjectManager, Command, ParticipantProject
-
+from scripts.form_groups import form_groups, distribute_students
+from automation.models import (
+    Project, Student,
+    ProjectManager, Command,
+    ParticipantProject
+)
 
 students = Student.objects.filter(category__name='Джуниор')
 students_new = Student.objects.filter(category__name='Новичек')
@@ -11,7 +14,6 @@ new_students = []
 
 for student in students:
     students_junior.append(student.id)
-
 
 for student in students_new:
     new_students.append(student.id)
@@ -28,29 +30,27 @@ def create_groups(pm_name, groups, project_name):
         for students in groups[commands]:
             pm = ProjectManager.objects.filter(first_name=pm_name).first()
             student = Student.objects.filter(id=students).first()
-            participants = ParticipantProject.objects.create(
+            ParticipantProject.objects.create(
                 student=student,
                 project_manager=pm,
                 command=command
             )
 
 
-junior = form_gorups(students_junior)
-new_students = form_gorups(new_students)
-junior_groups_for_tim_week_2 = junior['groups_for_tim_week_2']
-junior_groups_for_tim_week_1 = junior['groups_for_tim_week_1']
-junior_groups_for_kate_week_2 = junior['groups_for_kate_week_2']
-junior_groups_for_kate_week_1 = junior['groups_for_kate_week_1']
+tim_week_1_students_junior, kate_week_1_students_junior, kate_week_2_students_junior, tim_week_2_students_junior = distribute_students(
+    students_junior)
+tim_week_1_students_beginner, kate_week_1_students_beginner, kate_week_2_students_beginner, tim_week_2_students_beginner = distribute_students(
+    new_students)
 
-beginner_groups_for_tim_week_2 = new_students['groups_for_tim_week_2']
-beginner_groups_for_tim_week_1 = new_students['groups_for_tim_week_1']
-beginner_groups_for_kate_week_2 = new_students['groups_for_kate_week_2']
-beginner_groups_for_kate_week_1 = new_students['groups_for_kate_week_1']
+junior_groups_for_tim_week_1 = form_groups(tim_week_1_students_junior)
+junior_groups_for_kate_week_1 = form_groups(kate_week_1_students_junior)
+junior_groups_for_kate_week_2 = form_groups(kate_week_2_students_junior)
+junior_groups_for_tim_week_2 = form_groups(tim_week_2_students_junior)
 
-
-
-
-
+beginner_groups_for_tim_week_2 = form_groups(tim_week_1_students_beginner)
+beginner_groups_for_tim_week_1 = form_groups(kate_week_1_students_beginner)
+beginner_groups_for_kate_week_2 = form_groups(kate_week_2_students_beginner)
+beginner_groups_for_kate_week_1 = form_groups(tim_week_2_students_beginner)
 
 
 
