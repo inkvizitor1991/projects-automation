3fc6ce94612516bb3aea64733b4c7b18457bb0b0
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2022-02-03 20:27:45 +0300

    Reduced the algorithm

--- scripts/form_groups.py
+++ scripts/form_groups.py
@@ -1,42 +1,47 @@
-from scripts.build_team import build_team
-from scripts.distribution_students import distribution_students
+import copy
+
+from random import randint
 
 
 
+def chunks_generators(students, chunks_number):
+    for student in range(0, len(students), chunks_number):
+        yield students[student: student + chunks_number]
 
 
-def form_gorups(group):
-    students_number = len(group)
-    commands = build_team(students_number)
-    tim_all_student_1_week = group[:commands['tim']['tim_week_1']]
-    len_tim_all_student_1_week = len(tim_all_student_1_week)
-
-    tim_one_additional_group_week_1 = commands['tim']['tim_one_additional_group_week_1']
-    tim_one_extended_group_week_1 = commands['tim']['tim_one_extended_group_week_1']
-    tim_one_additional_group_week_2 = commands['tim']['tim_one_additional_group_week_2']
-    tim_one_extended_group_week_2 = commands['tim']['tim_one_extended_group_week_2']
-    tim_all_student_2_week = group[len_tim_all_student_1_week : commands['tim']['tim_all_student']]
-    kate_all_student_1_week = group[commands['tim']['tim_all_student']: commands['tim']['tim_all_student']+commands['kate']['kate_week_1']]
-    len_kate_all_student_1_week = int(commands['tim']['tim_all_student']+commands['kate']['kate_week_1'])
-    kate_all_student_2_week = group[len_kate_all_student_1_week:]
-    kate_one_additional_group_week_1 = commands['kate']['kate_one_additional_group_week_1']
-    kate_one_extended_group_week_1 = commands['kate']['kate_one_extended_group_week_1']
-
-    kate_one_additional_group_week_2 = commands['kate']['kate_one_additional_group_week_2']
-    kate_one_extended_group_week_2 = commands['kate']['kate_one_extended_group_week_2']
-
-    groups_for_tim_week_2 = distribution_students(tim_one_additional_group_week_2, tim_one_extended_group_week_2, tim_all_student_2_week)
-    groups_for_tim_week_1 = distribution_students(tim_one_additional_group_week_1, tim_one_extended_group_week_1, tim_all_student_1_week)
-
-    groups_for_kate_week_1 = distribution_students(kate_one_additional_group_week_1, kate_one_extended_group_week_1, kate_all_student_1_week)
-    groups_for_kate_week_2 = distribution_students(kate_one_additional_group_week_2, kate_one_extended_group_week_2, kate_all_student_2_week)
-    formed_groups = {'groups_for_tim_week_1':groups_for_tim_week_1,
-                     'groups_for_tim_week_2':groups_for_tim_week_2,
-                     'groups_for_kate_week_1':groups_for_kate_week_1,
-                     'groups_for_kate_week_2':groups_for_kate_week_2
-                     }
-    return formed_groups
+def distribute_students(students):
+    week_1_students_number = len(students) // 2
+    week_1_students = students[:week_1_students_number]
+    tim_week_1_students_number = week_1_students_number // 2
+    tim_week_1_students = students[:tim_week_1_students_number]
+    kate_week_1_students = students[tim_week_1_students_number:week_1_students_number]
+    week_2_students = students[week_1_students_number:]
+    kate_week_2_students_number = len(week_2_students) // 2
+    kate_week_2_students = week_2_students[:kate_week_2_students_number]
+    tim_week_2_students = week_2_students[kate_week_2_students_number:]
+    return tim_week_1_students, kate_week_1_students, kate_week_2_students, tim_week_2_students
 
 
+def add_unique_number(students_groups):
+    named_groups = {}
+    for group in students_groups:
+        group_name = randint(1, 333)
+        named_groups.update({group_name: group})
+    return named_groups
 
 
+def form_groups(distributed_students):
+    """Forms groups, the total number of people should not be less than 8."""
+
+    generated_groups = list(chunks_generators(distributed_students, 3))
+    groups = copy.deepcopy(generated_groups)
+
+    if len(groups[-1]) == 1:
+        united_two_last_groups = groups[-1] + groups[-2]
+        groups = groups[:-2]
+        groups.append(united_two_last_groups)
+        unique_groups = add_unique_number(groups)
+        return unique_groups
+    else:
+        unique_groups = add_unique_number(groups)
+        return unique_groups
